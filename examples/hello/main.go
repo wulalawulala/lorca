ccccf37415a5e24e6a62600e6cc2024be47cd4b8
qm <123736640@qq.com>
2022-09-21 20:38:41 +0800

    1、关闭--enable-automation的提示 2、新增测试demo使用--kiosk进行全屏浏览

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"log"
 	"net/url"
+	"runtime"
 
 	"github.com/zserge/lorca"
 )
 
 func main() {
+	args := []string{}
+	if runtime.GOOS == "linux" {
+		args = append(args, "--class=Lorca")
+	}
+	args = append(args, "--kiosk")
+
 	// Create UI with basic HTML passed via data URI
 	ui, err := lorca.New("data:text/html,"+url.PathEscape(`
 	<html>
 		<head><title>Hello</title></head>
 		<body><h1>Hello, world!</h1></body>
 	</html>
-	`), "", 480, 320)
+	`), "", 800, 600, args...)
 	if err != nil {
 		log.Fatal(err)
 	}
